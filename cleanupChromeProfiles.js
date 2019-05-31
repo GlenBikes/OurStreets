1a32b88ea97eb0167254511147a043451ff1e18d
Daniel Schep <dschep@gmail.com>
2019-05-31 10:04:31 -0400

    doh. forgot file

---
+++ cleanupChromeProfiles.js
@@ -0,0 +1,16 @@
+const glob = require('glob')
+
+const cleanupChromeProfiles = () => {
+    glob('/tmp/*', null, (err, files) => console.log('all files: ', files))
+    glob('/tmp/core.headless_shell*', null, (err, files) => {
+        if (err) console.log(err)
+        files.forEach(f => {
+            fs.unlink(f, (err) => {
+                if (err) console.log('Error deleting file, ', f)
+                console.log('file ', f, ' deleted')
+            })
+        })
+    })
+}
+
+module.exports = cleanupChromeProfiles
