faa140822d93ef6b11d579ad52003691842a8559
Daniel Schep <dschep@gmail.com>
2019-12-13 01:45:40 +0000

    try to speed up warm starts by keeping the browser

--- handler.js
+++ handler.js
@@ -1,22 +1,24 @@
 "use strict";
 const puppeteer = require("puppeteer");
 const lookupPlate = require("./lookupPlate");
-const cleanupChromeProfiles = require("./cleanupChromeProfiles");
+
+let browser;
 
 module.exports.index = async (event, context, callback) => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: process.env.IS_LOCAL ? undefined : "/opt/headless_shell",
-    args: ["--no-sandbox", "--disable-gpu", "--single-process"]
-  });
-
+  context.callbackWaitsForEmptyEventLoop = false;
+  if (!browser) {
+    browser = await puppeteer.launch({
+      headless: true,
+      executablePath: process.env.IS_LOCAL ? undefined : "/opt/headless_shell",
+      args: ["--no-sandbox", "--disable-gpu", "--single-process"]
+    });
+  }
+  
   let tries = 0;
   let res = { error: "captcha error" };
   while (res.error == "captcha error" && tries < 10) {
     res = await lookupPlate(browser, event.state, event.number);
     tries++;
   }
-  await browser.close();
-  cleanupChromeProfiles();
   return res;
 };
