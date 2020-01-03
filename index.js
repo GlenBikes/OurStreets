3358e64a98e206916a993a431e9d8cf5cc9335c0
Daniel Schep <dschep@gmail.com>
2020-01-02 20:34:59 -0500

    test & lint & CI!

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const EtimspaymentsLookup = require('./lib/etimspayments')
-const DspaymentsLookup = require('./lib/dspayments')
-const DcLookup = require('./lib/dc')
-const PghLookup = require('./lib/pgh')
+const EtimspaymentsLookup = require("./lib/etimspayments");
+const DspaymentsLookup = require("./lib/dspayments");
+const DcLookup = require("./lib/dc");
+const PghLookup = require("./lib/pgh");
+const SeaLookup = require("./lib/sea");
 
 module.exports = {
   EtimspaymentsLookup,
   DspaymentsLookup,
   DcLookup,
-  PghLookup
-}
+  PghLookup,
+  SeaLookup
+};
